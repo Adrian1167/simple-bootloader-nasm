547d7cf4de9cf79e81646564a989938ab9789c78
Adrian Jianu <adrianmjianu@gmail.com>
2025-06-10 21:58:32 +0300

    Added memory segments initialization and treat the BIOS Parameter Block

--- boot.asm
+++ boot.asm
@@ -1,27 +1,47 @@
-ORG 0x7c00
+ORG 0
 BITS 16
+
+_start:
+    jmp short start
+    nop
+
+times 33 db 0 ; Reserve space for BIOS Parameter Block
+
+start:
+    jmp 0x7c0:step2 ; Set Code Segment as 0x7c0
+
+step2:
+    cli
+    mov ax, 0x7c0 ; Initialize our memory segments inside critical area
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
 
 message: db 'Hello World!', 0
 
-jmp _init
-
-_init:
-    mov si, message
-    mov ah, 0eh
-    call _loop
-    jmp $
-
-_loop:
-    lodsb
-    cmp al, 0
-    je _done
-    int 10h
-    jmp _loop
-
-_done:
-    ret
-
-ret
-
-times 510-($ - $$) db 0
-dw 0xAA55
+times 510-($ - $$) db 0 ; Fill rest of the memory until byte 510 with 0s
+dw 0xAA55 ; BTLD signature
